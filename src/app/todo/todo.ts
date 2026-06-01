0a0a7f9cb4209f0aa70b76f5605a31bdcc17845d
Christian Liebel <christian.liebel@thinktecture.com>
2026-06-01 12:03:43 -0700

    Lab #2 and #3

--- src/app/todo/todo.ts
+++ src/app/todo/todo.ts
@@ -28,17 +28,37 @@ import {TODO_TOOL} from './tools';
 export class Todo implements OnInit {
   protected llmService = new LlmService();
   // LAB #3, #4
+  protected readonly reply = signal('');
 
   async ngOnInit() {
     // LAB #2
+    await this.llmService.loadModel('2.6B', 'q4');
   }
 
   async runPrompt(userPrompt: string, inferenceEngine: string) {
     // LAB #3, #9
+    this.reply.set('…');
+
+    const chunks = inferenceEngine === 'transformers-js'
+      ? this.inferTransformersJs(userPrompt)
+      : this.inferPromptApi(userPrompt);
+
+    let reply = '';
+    for await (const chunk of chunks) {
+      reply += chunk;
+      this.reply.set(reply);
+    }
   }
 
   inferTransformersJs(userPrompt: string) {
     // LAB #3, #6, #7, #8, #9
+    this.llmService.clearPastKeyValues();
+
+    const messages = [
+      { role: "user", content: userPrompt },
+    ];
+
+    return this.llmService.generateResponse(messages, []);
   }
 
   async* inferPromptApi(userPrompt: string): AsyncGenerator<string> {
